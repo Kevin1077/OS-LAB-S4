ac17c5af43994ab25a21b6e67695136153e950de
Kevin Biju Kulangara <118618402+Kevin1077@users.noreply.github.com>
2024-03-09 11:36:19 +0530

    Create details.sh

---
+++ details.sh
@@ -0,0 +1,17 @@
+echo Enter your details:
+echo "Enter your name:"
+read name
+echo "Enter your semester:"
+read sem
+echo "Enter your Batch:"
+read batch
+echo "Enter your roll no:"
+read rollno
+echo "Enter your Register number:"
+read regno
+echo "-------------------------------------"
+echo "name: $name"
+echo "semester: $sem"
+echo "Batch: $batch"
+echo "roll no: $rollno"
+echo "Register number: $regno"
